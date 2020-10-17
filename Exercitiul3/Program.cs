82f3a41593e5bf07c30c71ad347d7256471dfafd
irinaeris <72436931+irinaeris@users.noreply.github.com>
2020-10-17 17:48:42 +0300

    Add project files.

---
+++ Exercitiul3/Program.cs
@@ -0,0 +1,175 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//8.	Scrieti un program care sa afiseze primele 10 numere naturale.( do while)
+
+//namespace Exercitiul8
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+
+//        {
+//            int i = 0;
+//            do
+//            {
+//                Console.WriteLine(i);
+//                i++;
+//            }
+//            while (i < 10);
+//            Console.ReadLine();
+//        }
+
+//    }
+//}
+
+//9. Scrieti un program care sa afiseze suma primelor 10 numere naturale. ( do while)
+
+//namespace Exercitiul9
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+
+//        {
+//            int i = 0;
+//            int sum = 0;
+//            do
+//            {
+//                sum = sum + i;
+//                i++;
+//            }
+//            while (i <= 10);
+//            Console.WriteLine(sum);
+//            Console.ReadLine();
+//        }
+
+//    }
+//}
+
+//10.	Cititi un numar de la tastatura.Afisati toate numerele mai mici decat numarul citit de voi. .( do while)
+
+//namespace Exercitiul10
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int i = 1;
+//            Console.WriteLine("Enter value:");
+//            int n = Convert.ToInt32(Console.ReadLine());
+
+//            do
+//            {
+//                Console.WriteLine(i);
+//                i++;
+//            }
+//            while (i < n);
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//11. Cititi un numar de la tastatura.Afisati toate numerele de la 3 pana la numarul citit de voi. .( do while)
+
+//namespace Exercitiul11
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int i = 3;
+//            Console.WriteLine("Enter value:");
+//            int n = Convert.ToInt32(Console.ReadLine());
+
+//            do
+//            {
+//                Console.WriteLine(i);
+//                i++;
+//            }
+//            while (i <= n);
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//12. Cititi un numar de la tastatura.Afisati suma numerelor mai mici decat numarul citit de voi. .( do while)
+
+//namespace Exercitiul12
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int i = 0;
+//            int sum = 0;
+//            Console.WriteLine("Enter value:");
+//            int n = Convert.ToInt32(Console.ReadLine());
+
+//            do
+//            {
+//                sum = sum + i;
+//                i++;
+//            }
+//            while (i < n);
+//            Console.WriteLine(sum);
+//            Console.ReadLine();
+//        }
+
+//    }
+//}
+
+//13.	Afisati cubul primelor 5 numere naturale. .( do while)
+
+//namespace Exercitiul13
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int i = 0;
+//            int c = 1;
+
+//            do
+//            {
+//                c = i * i * i;
+//                Console.WriteLine(c);
+//                i++;
+//            }
+//            while (i < 5);
+//            Console.ReadLine();
+//        }
+
+//    }
+//}
+
+//14. Cititi un numar de la tastatura si afisati numerele pare mai mici decat numarul vostru. .( do while)
+
+//namespace Exercitiul7
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int i = 1;
+//            Console.WriteLine("Enter value:");
+//            int n = Convert.ToInt32(Console.ReadLine());
+
+//            do
+//            {
+//                if (i % 2 == 0)
+//                {
+//                    Console.WriteLine(i);
+//                    i++;
+//                }
+
+//            }
+//            while (i < n);
+//            Console.ReadLine();
+//        }
+       
+//    }
+//} 
